29acd080d96e902c277301c8c77d3d9c40e766f1
codeamac04 <codeamac04@gmail.com>
2016-06-07 13:40:22 -0500

    Metodos destructivos

---
+++ Destructive.rb
@@ -0,0 +1,11 @@
+calificaciones = [7,5,4,3,7,8,9,10]
+
+puts "Tus calificaciones son: #{calificaciones}"
+
+puts "Llamando al método sort en calificaciones : #{calificaciones.sort}"
+
+puts "Valor de calificaciones despues de pasarlo por sort: #{calificaciones}"
+
+puts "Llamando al método sort! en calificaciones : #{calificaciones.sort!}"
+
+puts "Valor de calificaciones después de pasarlo por sort!: #{calificaciones}"
